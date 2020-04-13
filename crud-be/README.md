0fde40cbb2c28733595f39041ea1aa6607ff3a5e
Leonid Vaitsehovich <lvaitsehovich@ptsecurity.com>
2020-04-13 16:24:13 +0300

    chore(config): Add swagger support

--- crud-be/README.md
+++ crud-be/README.md
@@ -1,1 +1,5 @@
 // win-node-env for environment setup on windows
+
+Run `yarn run build` to trigger swagger plugin.
+Run `yarn run start:dev` to run the app.
+Visil `localhost:5000/api` to get swagger info.
--- crud-be/src/main.ts
+++ crud-be/src/main.ts
@@ -1,11 +1,22 @@
 import * as config from 'config';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const server = process.env.SERVER_PORT || config.get('server');
 
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('Experimental BE example')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(server.port);
 }
 bootstrap();
